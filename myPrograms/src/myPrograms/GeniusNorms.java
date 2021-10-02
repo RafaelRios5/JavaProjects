30175e471f9ab2bccb6a9d643bf0c0f5a4fdf0f0
HGP45 <hgp45@THes-Macintosh.local>
2021-10-01 19:19:41 -0700

    Created New Program Called GeniusNorms

---
+++ myPrograms/src/myPrograms/GeniusNorms.java
@@ -0,0 +1,14 @@
+package myPrograms;
+
+public class GeniusNorms {
+	public static void main(String[] args) {
+		System.out.println("Our Community Norms");// Print Out Title
+		System.out.println("-----------------------------------------------------");// Divider
+		System.out.println("1.Rafael's Community Norm Be Respectfull");// Print Out Rafael's Community Norm
+		System.out.println("2.Sione's Community Norm Let each other know when we can't attend and why/ Teamwork");// Print Out Sione's Community Norm 
+		System.out.println("3.Vijay's Community Norm Help Others");// Print Out Vijay's Community Norm
+		System.out.println("4.Jonathon's Community Norm Communicate with everyone.");// Print Out Jonathon's Community Norm
+		System.out.println("-----------------------------------------------------");// Divider
+	}
+
+}
